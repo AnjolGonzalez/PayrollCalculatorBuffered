cff854c4dc4864175535f2ce5c5884ce21bb9dde
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-10-17 14:07:21 -0400

    "Final Commit"

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,2 +1,49 @@
-package com.pluralsight;public class Employee {
-}
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+    private double grossPay;
+
+    public Employee(int id, String name, double hours, double payTime) {
+        this.employeeId = id;
+        this.name = name;
+        this.hoursWorked = hours;
+        this.payRate = payTime;
+        this.grossPay = grossPay;
+    }
+
+    public int getEmployeeId (){
+        return employeeId;
+    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    public double getPayRate() {
+        return payRate;
+    }
+    public void setPayRate(double payRate){
+        this.payRate = payRate;
+    }
+    public void setGrossPay(double grossPay) {
+        this.grossPay = grossPay;
+    }
+    public double getgrossPay(){
+        return hoursWorked * payRate;
+        }
+    }
+
--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,2 +1,34 @@
-package com.pluralsight;public class PayrollCalculator {
+package com.pluralsight;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String input;
+
+            while ((input = bufferedReader.readLine()) != null){
+
+            String[] employee = input.split("\\|");
+            int id = Integer.parseInt(employee[0]);
+            String name = employee[1];
+            double hoursWorked = Double.parseDouble(employee[2]);
+            double payRate = Double.parseDouble(employee[3]);
+
+            Employee employees = new Employee(id, name, hoursWorked, payRate);
+
+            System.out.printf("Employee ID: %s,Name: %s, GrossPay: $%.2f%n", employees.getEmployeeId(), employees.getName(), employees.getgrossPay());
+        }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Error!\n" +
+                    "Try again:");
+            e.printStackTrace();
+        }
+
+
+    }
 }
