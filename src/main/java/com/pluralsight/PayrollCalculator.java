7063970c196d960467220cc32f2533854f720e16
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-10-18 10:47:15 -0400

    "last Commit"

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,34 +1,63 @@
 package com.pluralsight;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
-        try {
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+        Scanner scan = new Scanner(System.in);
+        String fileChose;
+        String fileMade;
+        System.out.print("Enter the name of file of employee that you want to process: ");
+        fileChose = scan.nextLine();
+        System.out.println("Enter the name of the file you want to create");
+        fileMade = scan.nextLine();
+
+        while(fileMade.equals("employees.csv")){
+            System.out.println("ERROR! ERROR!\n" +
+                    "Try again:\n" +
+                    "ENTER HERE: ");
+
+            if (fileMade.equals("employees.csv")) {
+                System.out.println("Cannot overwrite existing file 'employees.csv', please try again.");
+            }
+            fileMade = scan.nextLine();
+        }
+
+        try{
+            FileReader fileReader = new FileReader("src/main/resources/" + fileChose );
+            BufferedReader buffRead = new BufferedReader(fileReader);
             String input;
 
-            while ((input = bufferedReader.readLine()) != null){
+            FileWriter fileWriter = new FileWriter(fileMade);
 
-            String[] employee = input.split("\\|");
-            int id = Integer.parseInt(employee[0]);
-            String name = employee[1];
-            double hoursWorked = Double.parseDouble(employee[2]);
-            double payRate = Double.parseDouble(employee[3]);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            int printOnce = 0;
+            while((input = buffRead.readLine()) != null){
 
-            Employee employees = new Employee(id, name, hoursWorked, payRate);
+                String[] employee = input.split("\\|");
 
-            System.out.printf("Employee ID: %s,Name: %s, GrossPay: $%.2f%n", employees.getEmployeeId(), employees.getName(), employees.getgrossPay());
+                int id = Integer.parseInt(employee[0]);
+                String name = employee[1];
+                double hoursWorked = Double.parseDouble(employee[2]);
+                double payRate = Double.parseDouble(employee[3]);
+                Employee employees = new Employee(id ,name, (int) hoursWorked,payRate);
+                double grossPay = employees.getgrossPay();
+                String formattedGrossPay = String.format("%.2f", grossPay);
+                System.out.printf("Employee ID: %d | Name: %s | Gross Pay: $%.2f%n", id , name, employees.getgrossPay());
+                if (printOnce < 1){
+                    bufWriter.write("id|name|hours-worked|pay-rate \n");
+                    printOnce++;
+                }
+                bufWriter.write(id + "|" + name + "|" + "$" + formattedGrossPay);
+                bufWriter.newLine();
+            }
+            buffRead.close();
+            bufWriter.close();
+
         }
-            bufferedReader.close();
-        } catch (IOException e) {
-            System.out.println("Error!\n" +
-                    "Try again:");
+        catch (IOException e){
             e.printStackTrace();
         }
-
 
     }
 }
